b4a8cc36937404a4876d675e4b940985cbb79398
kv2 <cheng.kv2@gmail.com>
2021-08-04 08:59:25 +0800

    [*]优化dns服务的输出名称

--- gonmap.go
+++ gonmap.go
@@ -135,6 +135,9 @@ func (n *Nmap) Scan(ip string, port int) TcpBanner {
 	if b.TcpFinger.Service == "ms-wbt-server" {
 		b.TcpFinger.Service = "rdp"
 	}
+	if b.TcpFinger.Service == "domain" {
+		b.TcpFinger.Service = "dns"
+	}
 	if b.TcpFinger.Service == "ssl" && n.target.port == 3389 {
 		b.TcpFinger.Service = "rdp"
 	}
