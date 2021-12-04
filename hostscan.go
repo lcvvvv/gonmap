f662662879d99253cb33988970b9f8705f97ef72
kv2 <cheng.kv2@gmail.com>
2021-12-04 10:26:56 +0800

    [*]优化冗余代码

--- hostscan.go
+++ hostscan.go
@@ -6,32 +6,28 @@ import (
 	"time"
 )
 
-func HostDiscovery(ip string) (online bool) {
-	online = ping.Check(ip)
-	if online {
+func HostDiscovery(ip string) bool {
+	if HostDiscoveryForIcmp(ip) {
 		return true
 	}
-	online = tcpCheck(ip)
-	if online {
-		return true
-	}
-	return false
-}
-
-func HostDiscoveryIcmp(ip string) (online bool) {
-	online = ping.Check(ip)
-	if online {
+	if HostDiscoveryForTcp(ip) {
 		return true
 	}
 	return false
 }
 
-func tcpCheck(ip string) bool {
-	tcpArr := []int{21, 22, 23, 80, 443, 445, 8080, 3389}
+func HostDiscoveryForIcmp(ip string) bool {
+	if ping.Check(ip) {
+		return true
+	}
+	return false
+}
+
+func HostDiscoveryForTcp(ip string) bool {
+	tcpArr := []int{22, 23, 80, 139, 443, 445, 3389}
 	for _, port := range tcpArr {
 		netloc := fmt.Sprintf("%s:%d", ip, port)
-		online := PortScan("tcp", netloc, time.Second*2)
-		if online {
+		if PortScan("tcp", netloc, time.Second*2) {
 			return true
 		}
 	}
