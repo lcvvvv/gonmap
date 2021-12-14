46b31d15e146e7692582fe221c5b88888fb1404e
kv2 <cheng.kv2@gmail.com>
2021-12-14 22:29:33 +0800

    [*]优化书写格式,使其更美观

--- simplenet/simplenet.go
+++ simplenet/simplenet.go
@@ -16,21 +16,19 @@ func Send(protocol string, netloc string, data string, duration time.Duration, s
 		//fmt.Println(conn)
 		return "", errors.New(err.Error() + " STEP1:CONNECT")
 	}
+	defer conn.Close()
 	buf := make([]byte, size)
 	_, err = io.WriteString(conn, data)
 	//_, err = io.WriteString(conn, data)
 	if err != nil {
-		_ = conn.Close()
 		return "", errors.New(err.Error() + " STEP2:WRITE")
 	}
 	//设置读取超时Deadline
 	_ = conn.SetReadDeadline(time.Now().Add(duration * 2))
 	length, err := conn.Read(buf)
 	if err != nil && err.Error() != "EOF" {
-		_ = conn.Close()
 		return "", errors.New(err.Error() + " STEP3:READ")
 	}
-	_ = conn.Close()
 	if length == 0 {
 		return "", errors.New("STEP3:response is empty")
 	}
@@ -51,19 +49,17 @@ func TLSSend(protocol string, netloc string, data string, duration time.Duration
 	if err != nil {
 		return "", errors.New(err.Error() + " STEP1:CONNECT")
 	}
+	defer conn.Close()
 	_, err = io.WriteString(conn, data)
 	if err != nil {
-		_ = conn.Close()
 		return "", errors.New(err.Error() + " STEP2:WRITE")
 	}
 	buf := make([]byte, size)
 	_ = conn.SetReadDeadline(time.Now().Add(duration * 2))
 	length, err := conn.Read(buf)
 	if err != nil && err.Error() != " EOF" {
-		_ = conn.Close()
 		return "", errors.New(err.Error() + " STEP3:READ")
 	}
-	_ = conn.Close()
 	if length == 0 {
 		return "", errors.New("response is empty")
 	}
