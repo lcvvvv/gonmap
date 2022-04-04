8c9aec0167ec4131d63fbc50598e213ebc989d35
kv2 <cheng.kv2@gmail.com>
2022-04-04 18:12:43 +0800

    [*]优化gonmap代码逻辑结构

--- gonmap.go
+++ gonmap.go
@@ -1,10 +1,8 @@
 package gonmap
 
 import (
-	"errors"
 	"log"
 	"os"
-	"strconv"
 	"strings"
 	"time"
 )
@@ -16,13 +14,26 @@ var SSLSecondProbeMap = []string{"TCP_TerminalServerCookie", "TCP_TerminalServer
 var AllProbeMap = []string{"TCP_GetRequest", "TCP_NULL"}
 var SSLProbeMap = []string{"TCP_TLSSessionReq", "TCP_SSLSessionReq", "TCP_SSLv23SessionReq"}
 
+var ProbesCount = 0     //探针数
+var MatchCount = 0      //指纹数
+var UsedProbesCount = 0 //已使用探针数
+var UsedMatchCount = 0  //已使用探针数
+
 var logger = Logger(log.New(os.Stderr, "[gonmap] ", log.Ldate|log.Ltime|log.Lshortfile))
 
+type Logger interface {
+	Printf(format string, v ...interface{})
+	Println(v ...interface{})
+}
+
+func SetLogger(v Logger) {
+	logger = v
+}
+
 //r["PROBE"] 总探针数、r["MATCH"] 总指纹数 、r["USED_PROBE"] 已使用探针数、r["USED_MATCH"] 已使用指纹数
-func Init(filter int, timeout time.Duration) map[string]int {
+func Init(filter int) {
 	//初始化NMAP探针库
-	InitNMAP()
-	//fmt.Println("初始化了")
+	repairNMAPString()
 	NMAP = &Nmap{
 		exclude:        newPort(),
 		probeGroup:     make(map[string]*probe),
@@ -42,12 +53,51 @@ func Init(filter int, timeout time.Duration) map[string]int {
 	NMAP.loads(NMAP_SERVICE_PROBES + NMAP_CUSTOMIZE_PROBES)
 	//修复fallback
 	NMAP.fixFallback()
-	//将TCP_GetRequest的fallback参数设置为NULL探针，避免漏资产
-	NMAP.probeGroup["TCP_GetRequest"].fallback = "TCP_NULL"
-	NMAP.probeGroup["TCP_TerminalServerCookie"].fallback = "TCP_GetRequest"
-	NMAP.probeGroup["TCP_TerminalServer"].fallback = "TCP_GetRequest"
-	//配置超时时间
-	NMAP.setTimeout(timeout)
+	//新增自定义指纹信息
+	customNMAPMatch()
+	//优化检测逻辑，及端口对应的默认探针
+	optimizeNMAPProbes()
+	//输出统计数据状态
+	statistical()
+}
+
+func statistical() {
+	ProbesCount = len(NMAP.probeSort)
+	for _, p := range NMAP.probeGroup {
+		MatchCount += len(p.matchGroup)
+	}
+	UsedProbesCount = len(NMAP.portProbeMap[0])
+	for _, p := range NMAP.portProbeMap[0] {
+		UsedMatchCount += len(NMAP.probeGroup[p].matchGroup)
+	}
+}
+
+func repairNMAPString() {
+	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, "${backquote}", "`")
+	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `q|GET / HTTP/1.0\r\n\r\n|`,
+		`q|GET / HTTP/1.0\r\nUser-Agent: Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)\r\nAccept: */*\r\n\r\n|`)
+	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `\1`, `$1`)
+	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `(?=\\)`, `(?:\\)`)
+	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `(?=[\w._-]{5,15}\r?\n$)`, `(?:[\w._-]{5,15}\r?\n$)`)
+	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `(?:[^\r\n]*r\n(?!\r\n))*?`, `(?:[^\r\n]+\r\n)*?`)
+	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `(?:[^\r\n]*\r\n(?!\r\n))*?`, `(?:[^\r\n]+\r\n)*?`)
+	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `(?:[^\r\n]+\r\n(?!\r\n))*?`, `(?:[^\r\n]+\r\n)*?`)
+	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `(?!2526)`, ``)
+	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `(?!400)`, ``)
+	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `(?!\0\0)`, ``)
+	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `(?!/head>)`, ``)
+	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `(?!HTTP|RTSP|SIP)`, ``)
+	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `(?!.*[sS][sS][hH]).*`, `.*`)
+	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `(?!\xff)`, `.`)
+	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `(?!x)`, `[^x]`)
+	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `(?<=.)`, `(?:.)`)
+	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `(?<=\?)`, `(?:\?)`)
+	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `\x20\x02\x00.`, `\x20\x02..`)
+	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `match rtmp`, `# match rtmp`)
+	NMAP_SERVICE_PROBES = ReplaceAll(NMAP_SERVICE_PROBES, `nmap`, `pamn`)
+}
+
+func customNMAPMatch() {
 	//新增自定义指纹信息
 	NMAP.AddMatch("TCP_GetRequest", `echo m|^GET / HTTP/1.0\r\n\r\n$|s`)
 	NMAP.AddMatch("TCP_GetRequest", `mongodb m|.*It looks like you are trying to access MongoDB.*|s p/MongoDB/`)
@@ -77,6 +127,10 @@ func Init(filter int, timeout time.Duration) map[string]int {
 	NMAP.AddMatch("TCP_NULL", `telnet m|^.*Welcome Visiting Huawei Home Gateway\x0d\x0aCopyright by Huawei Technologies Co., Ltd.*Login:|s p/Huawei/`)
 	NMAP.AddMatch("TCP_NULL", `telnet m|^..\x01..\x03..\x18..\x1f|s p/Huawei/`)
 	NMAP.AddMatch("TCP_TerminalServerCookie", `ms-wbt-server m|^\x03\0\0\x13\x0e\xd0\0\0\x124\0\x02.*\0\x02\0\0\0| p/Microsoft Terminal Services/ o/Windows/ cpe:/o:microsoft:windows/a`)
+
+}
+
+func optimizeNMAPProbes() {
 	//优化检测逻辑，及端口对应的默认探针
 	NMAP.portProbeMap[3390] = append(NMAP.portProbeMap[3390], "TCP_TerminalServer")
 	NMAP.portProbeMap[3390] = append(NMAP.portProbeMap[3390], "TCP_TerminalServerCookie")
@@ -90,32 +144,10 @@ func Init(filter int, timeout time.Duration) map[string]int {
 	NMAP.portProbeMap[2022] = append(NMAP.portProbeMap[2022], "TCP_Socks5")
 	NMAP.portProbeMap[6000] = append(NMAP.portProbeMap[6000], "TCP_Socks5")
 	NMAP.portProbeMap[7000] = append(NMAP.portProbeMap[7000], "TCP_Socks5")
-	return NMAP.Status()
-}
-
-func InitNMAP() {
-	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, "${backquote}", "`")
-	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `q|GET / HTTP/1.0\r\n\r\n|`,
-		`q|GET / HTTP/1.0\r\nUser-Agent: Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)\r\nAccept: */*\r\n\r\n|`)
-	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `\1`, `$1`)
-	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `(?=\\)`, `(?:\\)`)
-	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `(?=[\w._-]{5,15}\r?\n$)`, `(?:[\w._-]{5,15}\r?\n$)`)
-	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `(?:[^\r\n]*r\n(?!\r\n))*?`, `(?:[^\r\n]+\r\n)*?`)
-	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `(?:[^\r\n]*\r\n(?!\r\n))*?`, `(?:[^\r\n]+\r\n)*?`)
-	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `(?:[^\r\n]+\r\n(?!\r\n))*?`, `(?:[^\r\n]+\r\n)*?`)
-	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `(?!2526)`, ``)
-	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `(?!400)`, ``)
-	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `(?!\0\0)`, ``)
-	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `(?!/head>)`, ``)
-	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `(?!HTTP|RTSP|SIP)`, ``)
-	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `(?!.*[sS][sS][hH]).*`, `.*`)
-	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `(?!\xff)`, `.`)
-	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `(?!x)`, `[^x]`)
-	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `(?<=.)`, `(?:.)`)
-	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `(?<=\?)`, `(?:\?)`)
-	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `\x20\x02\x00.`, `\x20\x02..`)
-	NMAP_SERVICE_PROBES = strings.ReplaceAll(NMAP_SERVICE_PROBES, `match rtmp`, `# match rtmp`)
-	NMAP_SERVICE_PROBES = ReplaceAll(NMAP_SERVICE_PROBES, `nmap`, `pamn`)
+	//将TCP_GetRequest的fallback参数设置为NULL探针，避免漏资产
+	NMAP.probeGroup["TCP_GetRequest"].fallback = "TCP_NULL"
+	NMAP.probeGroup["TCP_TerminalServerCookie"].fallback = "TCP_GetRequest"
+	NMAP.probeGroup["TCP_TerminalServer"].fallback = "TCP_GetRequest"
 }
 
 func New() *Nmap {
@@ -124,317 +156,12 @@ func New() *Nmap {
 	return n
 }
 
-type Nmap struct {
-	exclude        *port
-	probeGroup     map[string]*probe
-	probeSort      []string
-	probeFilter    int
-	portProbeMap   map[int][]string
-	usedProbeSlice []string
-
-	target target
-	filter int
-
-	response response
-	finger   *TcpFinger
-}
-
-func (n *Nmap) Scan(ip string, port int) TcpBanner {
-	n.target.host = ip
-	n.target.port = port
-
-	//拼接端口探测队列，全端口探测放在最后
-	b := NewTcpBanner(ip, port)
-
-	//对特殊端口优先发起特定探针
-	if IsInIntArr(BypassAllProbePortMap, port) {
-		b.Load(n.ScanByProbeSlice(n.portProbeMap[port]))
-		if b.status == Closed || (b.status == Matched && b.TcpFinger.Service != "ssl") {
-			return b
-		}
-	} else { //非特殊端口，会优先进行全局探针测试
-		//开始使用全局探针进行测试
-		b.Load(n.ScanByProbeSlice(AllProbeMap))
-		if b.status == Closed || (b.status == Matched && b.TcpFinger.Service != "ssl") {
-			return b
-		}
-		//开始进行特定探针测试
-		b.Load(n.ScanByProbeSlice(n.portProbeMap[port]))
-		if b.status == Closed || (b.status == Matched && b.TcpFinger.Service != "ssl") {
-			return b
-		}
-	}
-
-	//开始SSL探针测试
-	if b.TcpFinger.Service != "ssl" {
-		b.Load(n.ScanByProbeSlice(SSLProbeMap))
-		if b.status == Closed {
-			return b
-		}
-	}
-
-	if b.status == Matched {
-		if b.TcpFinger.Service != "ssl" {
-			return b
-		}
-		b.Load(n.ScanByProbeSlice(SSLSecondProbeMap))
-
-		if b.TcpFinger.Service == "http" {
-			b.TcpFinger.Service = "https"
-		}
-
-		if b.status == Closed || b.status == Matched {
-			return b
-		}
-	}
-
-	return b
-}
-
-func (n *Nmap) getTcpBanner(p *probe) *TcpBanner {
-	b := NewTcpBanner(n.target.host, n.target.port)
-
-	tls := p.sslports.Exist(n.target.port)
-
-	data, err := p.scan(n.target, tls)
-
-	if err != nil {
-		logger.Println(data, err)
-	}
-	if err != nil {
-		b.ErrorMsg = err
-		if strings.Contains(err.Error(), "STEP1") {
-			//logger.Println(err.Error())
-			if n.target.port == 137 || n.target.port == 161 {
-				return b.OPEN()
-			}
-			return b.CLOSED()
-		}
-		//if p.request.protocol == "UDP" {
-		//	return b.CLOSED()
-		//}
-		return b.OPEN()
-	}
-
-	b.Response.string = data
-	//若存在返回包，则开始捕获指纹
-
-	b.TcpFinger = n.getFinger(data, p.request.name)
-
-	if b.TcpFinger.Service == "" {
-		return b.OPEN()
-	} else {
-		if tls {
-			if b.TcpFinger.Service == "http" {
-				b.TcpFinger.Service = "https"
-			}
-		}
-		return b.MATCHED()
-	}
-	//如果成功匹配指纹，则直接返回指纹
-}
-
-func (n *Nmap) AddMatch(probeName string, expr string) {
-	n.probeGroup[probeName].loadMatch(expr, false)
-}
-
-func (n *Nmap) Status() map[string]int {
-	r := make(map[string]int)
-	r["PROBE"] = len(NMAP.probeSort)
-	r["MATCH"] = 0
-	for _, p := range NMAP.probeGroup {
-		r["MATCH"] += len(p.matchGroup)
-	}
-	r["USED_PROBE"] = len(NMAP.portProbeMap[0])
-	r["USED_MATCH"] = 0
-	for _, p := range NMAP.portProbeMap[0] {
-		r["USED_MATCH"] += len(NMAP.probeGroup[p].matchGroup)
-	}
-	return r
-}
-
-func (n *Nmap) setTimeout(timeout time.Duration) {
-	if timeout == 0 {
-		return
-	}
-	for _, p := range n.probeGroup {
-		p.totalwaitms = timeout
-		p.tcpwrappedms = timeout
-	}
-}
-
-func (n *Nmap) isCommand(line string) bool {
-	//删除注释行和空行
-	if len(line) < 2 {
-		return false
-	}
-	if line[:1] == "#" {
-		return false
-	}
-	//删除异常命令
-	commandName := line[:strings.Index(line, " ")]
-	commandArr := []string{
-		"Exclude", "Probe", "match", "softmatch", "ports", "sslports", "totalwaitms", "tcpwrappedms", "rarity", "fallback",
-	}
-	for _, item := range commandArr {
-		if item == commandName {
-			return true
-		}
-	}
-	return false
-}
-
-func (n *Nmap) getFinger(data string, requestName string) *TcpFinger {
-	data = n.convResponse(data)
-
-	f := n.probeGroup[requestName].match(data)
-	//标记当前探针名称
-	f.ProbeName = requestName
-
-	if f.Service != "" || n.probeGroup[requestName].fallback == "" {
-		return f
-	}
-
-	fallback := n.probeGroup[requestName].fallback
-	for fallback != "" {
-		logger.Println("fallback:", fallback)
-		f = n.probeGroup[fallback].match(data)
-		fallback = n.probeGroup[fallback].fallback
-		if f.Service != "" {
-			break
-		}
-	}
-	return f
-}
-
-func (n *Nmap) convResponse(s1 string) string {
-	//	为了适配go语言的沙雕正则，只能讲二进制强行转换成UTF-8
-	b1 := []byte(s1)
-	var r1 []rune
-	for _, i := range b1 {
-		r1 = append(r1, rune(i))
-	}
-	s2 := string(r1)
-	return s2
-}
-
-func (n *Nmap) loads(s string) {
-	lines := strings.Split(s, "\n")
-	var probeArr []string
-	p := newProbe()
-	for _, line := range lines {
-		if !n.isCommand(line) {
-			continue
-		}
-		commandName := line[:strings.Index(line, " ")]
-		if commandName == "Exclude" {
-			n.loadExclude(line)
-			continue
-		}
-		if commandName == "Probe" {
-			if len(probeArr) != 0 {
-				p.loads(probeArr)
-				n.pushProbe(p)
-			}
-			probeArr = []string{}
-			p.Clean()
-		}
-		probeArr = append(probeArr, line)
-	}
-	p.loads(probeArr)
-	n.pushProbe(p)
-}
-
-func (n *Nmap) loadExclude(expr string) {
-	var exclude = newPort()
-	expr = expr[strings.Index(expr, " ")+1:]
-	for _, s := range strings.Split(expr, ",") {
-		if !exclude.Load(s) {
-			panic(errors.New("exclude 语句格式错误"))
-		}
-	}
-	n.exclude = exclude
-}
-
-func (n *Nmap) pushProbe(p *probe) {
-	PROBE := newProbe()
-	*PROBE = *p
-
-	n.probeSort = append(n.probeSort, p.request.name)
-	n.probeGroup[p.request.name] = PROBE
-
-	//建立端口扫描对应表，将根据端口号决定使用何种请求包
-	//如果端口列表为空，则为全端口
-	if p.rarity > n.filter {
-		return
-	}
-	//0记录所有使用的探针
-	n.portProbeMap[0] = append(n.portProbeMap[0], p.request.name)
-
-	//分别压入sslports,ports
-	for _, i := range p.ports.value {
-		n.portProbeMap[i] = append(n.portProbeMap[i], p.request.name)
-	}
-	for _, i := range p.sslports.value {
-		n.portProbeMap[i] = append(n.portProbeMap[i], p.request.name)
-	}
-
-}
-
-func (n *Nmap) ScanByProbeSlice(probeSlice []string) *TcpBanner {
-	b := NewTcpBanner(n.target.host, n.target.port)
-	for _, requestName := range probeSlice {
-		if IsInStrArr(n.usedProbeSlice, requestName) {
-			continue
-		}
-		b.Load(n.getTcpBanner(n.probeGroup[requestName]))
-		//如果端口未开放，则等待10s后重新连接
-		if b.status == Closed {
-			time.Sleep(time.Second * 10)
-			b.Load(n.getTcpBanner(n.probeGroup[requestName]))
-		}
-		logger.Printf("Target:%s,Probe:%s,Status:%s,Service:%s,Response:%s", b.Target.URI(), requestName, b.StatusDisplay(), b.TcpFinger.Service, strconv.Quote(b.Response.string))
-		if b.status == Closed || b.status == Matched {
-			break
-		}
-		if n.target.port == 53 {
-			if DnsScan(n.target.URI()) {
-				b.TcpFinger.Service = "dns"
-				b.Response.string = "dns"
-				b.MATCHED()
-			} else {
-				b.CLOSED()
-			}
-			break
-		}
-	}
-	return &b
-}
-
-func (n *Nmap) fixFallback() {
-	for probeName, probeType := range n.probeGroup {
-		fallback := probeType.fallback
-		if fallback == "" {
-			continue
-		}
-		if _, ok := n.probeGroup["TCP_"+fallback]; ok {
-			n.probeGroup[probeName].fallback = "TCP_" + fallback
-		} else {
-			n.probeGroup[probeName].fallback = "UDP_" + fallback
-		}
-	}
-}
-
 func SetScanVersion() {
 	//-sV参数深度解析
 	AllProbeMap = NMAP.probeSort
 }
 
-type Logger interface {
-	Printf(format string, v ...interface{})
-	Println(v ...interface{})
-}
-
-func SetLogger(v Logger) {
-	logger = v
+func SetTimeout(timeout time.Duration) {
+	//配置超时时间
+	NMAP.setTimeout(timeout)
 }
---
+++ type-nmap.go
@@ -0,0 +1,294 @@
+package gonmap
+
+import (
+	"errors"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type Nmap struct {
+	exclude        *port
+	probeGroup     map[string]*probe
+	probeSort      []string
+	probeFilter    int
+	portProbeMap   map[int][]string
+	usedProbeSlice []string
+
+	target target
+	filter int
+
+	response response
+	finger   *TcpFinger
+}
+
+func (n *Nmap) Scan(ip string, port int) TcpBanner {
+	n.target.host = ip
+	n.target.port = port
+
+	//拼接端口探测队列，全端口探测放在最后
+	b := NewTcpBanner(ip, port)
+
+	//对特殊端口优先发起特定探针
+	if IsInIntArr(BypassAllProbePortMap, port) {
+		b.Load(n.ScanByProbeSlice(n.portProbeMap[port]))
+		if b.status == Closed || (b.status == Matched && b.TcpFinger.Service != "ssl") {
+			return b
+		}
+	} else { //非特殊端口，会优先进行全局探针测试
+		//开始使用全局探针进行测试
+		b.Load(n.ScanByProbeSlice(AllProbeMap))
+		if b.status == Closed || (b.status == Matched && b.TcpFinger.Service != "ssl") {
+			return b
+		}
+		//开始进行特定探针测试
+		b.Load(n.ScanByProbeSlice(n.portProbeMap[port]))
+		if b.status == Closed || (b.status == Matched && b.TcpFinger.Service != "ssl") {
+			return b
+		}
+	}
+
+	//开始SSL探针测试
+	if b.TcpFinger.Service != "ssl" {
+		b.Load(n.ScanByProbeSlice(SSLProbeMap))
+		if b.status == Closed {
+			return b
+		}
+	}
+
+	if b.status == Matched {
+		if b.TcpFinger.Service != "ssl" {
+			return b
+		}
+		b.Load(n.ScanByProbeSlice(SSLSecondProbeMap))
+
+		if b.TcpFinger.Service == "http" {
+			b.TcpFinger.Service = "https"
+		}
+
+		if b.status == Closed || b.status == Matched {
+			return b
+		}
+	}
+
+	return b
+}
+
+func (n *Nmap) getTcpBanner(p *probe) *TcpBanner {
+	b := NewTcpBanner(n.target.host, n.target.port)
+
+	tls := p.sslports.Exist(n.target.port)
+
+	data, err := p.scan(n.target, tls)
+
+	if err != nil {
+		logger.Println(data, err)
+	}
+	if err != nil {
+		b.ErrorMsg = err
+		if strings.Contains(err.Error(), "STEP1") {
+			//logger.Println(err.Error())
+			if n.target.port == 137 || n.target.port == 161 {
+				return b.OPEN()
+			}
+			return b.CLOSED()
+		}
+		//if p.request.protocol == "UDP" {
+		//	return b.CLOSED()
+		//}
+		return b.OPEN()
+	}
+
+	b.Response.string = data
+	//若存在返回包，则开始捕获指纹
+
+	b.TcpFinger = n.getFinger(data, p.request.name)
+
+	if b.TcpFinger.Service == "" {
+		return b.OPEN()
+	} else {
+		if tls {
+			if b.TcpFinger.Service == "http" {
+				b.TcpFinger.Service = "https"
+			}
+		}
+		return b.MATCHED()
+	}
+	//如果成功匹配指纹，则直接返回指纹
+}
+
+func (n *Nmap) AddMatch(probeName string, expr string) {
+	n.probeGroup[probeName].loadMatch(expr, false)
+}
+
+func (n *Nmap) setTimeout(timeout time.Duration) {
+	if timeout == 0 {
+		return
+	}
+	for _, p := range n.probeGroup {
+		p.totalwaitms = timeout
+		p.tcpwrappedms = timeout
+	}
+}
+
+func (n *Nmap) isCommand(line string) bool {
+	//删除注释行和空行
+	if len(line) < 2 {
+		return false
+	}
+	if line[:1] == "#" {
+		return false
+	}
+	//删除异常命令
+	commandName := line[:strings.Index(line, " ")]
+	commandArr := []string{
+		"Exclude", "Probe", "match", "softmatch", "ports", "sslports", "totalwaitms", "tcpwrappedms", "rarity", "fallback",
+	}
+	for _, item := range commandArr {
+		if item == commandName {
+			return true
+		}
+	}
+	return false
+}
+
+func (n *Nmap) getFinger(data string, requestName string) *TcpFinger {
+	data = n.convResponse(data)
+
+	f := n.probeGroup[requestName].match(data)
+	//标记当前探针名称
+	f.ProbeName = requestName
+
+	if f.Service != "" || n.probeGroup[requestName].fallback == "" {
+		return f
+	}
+
+	fallback := n.probeGroup[requestName].fallback
+	for fallback != "" {
+		logger.Println("fallback:", fallback)
+		f = n.probeGroup[fallback].match(data)
+		fallback = n.probeGroup[fallback].fallback
+		if f.Service != "" {
+			break
+		}
+	}
+	return f
+}
+
+func (n *Nmap) convResponse(s1 string) string {
+	//	为了适配go语言的沙雕正则，只能讲二进制强行转换成UTF-8
+	b1 := []byte(s1)
+	var r1 []rune
+	for _, i := range b1 {
+		r1 = append(r1, rune(i))
+	}
+	s2 := string(r1)
+	return s2
+}
+
+func (n *Nmap) loads(s string) {
+	lines := strings.Split(s, "\n")
+	var probeArr []string
+	p := newProbe()
+	for _, line := range lines {
+		if !n.isCommand(line) {
+			continue
+		}
+		commandName := line[:strings.Index(line, " ")]
+		if commandName == "Exclude" {
+			n.loadExclude(line)
+			continue
+		}
+		if commandName == "Probe" {
+			if len(probeArr) != 0 {
+				p.loads(probeArr)
+				n.pushProbe(p)
+			}
+			probeArr = []string{}
+			p.Clean()
+		}
+		probeArr = append(probeArr, line)
+	}
+	p.loads(probeArr)
+	n.pushProbe(p)
+}
+
+func (n *Nmap) loadExclude(expr string) {
+	var exclude = newPort()
+	expr = expr[strings.Index(expr, " ")+1:]
+	for _, s := range strings.Split(expr, ",") {
+		if !exclude.Load(s) {
+			panic(errors.New("exclude 语句格式错误"))
+		}
+	}
+	n.exclude = exclude
+}
+
+func (n *Nmap) pushProbe(p *probe) {
+	PROBE := newProbe()
+	*PROBE = *p
+
+	n.probeSort = append(n.probeSort, p.request.name)
+	n.probeGroup[p.request.name] = PROBE
+
+	//建立端口扫描对应表，将根据端口号决定使用何种请求包
+	//如果端口列表为空，则为全端口
+	if p.rarity > n.filter {
+		return
+	}
+	//0记录所有使用的探针
+	n.portProbeMap[0] = append(n.portProbeMap[0], p.request.name)
+
+	//分别压入sslports,ports
+	for _, i := range p.ports.value {
+		n.portProbeMap[i] = append(n.portProbeMap[i], p.request.name)
+	}
+	for _, i := range p.sslports.value {
+		n.portProbeMap[i] = append(n.portProbeMap[i], p.request.name)
+	}
+
+}
+
+func (n *Nmap) ScanByProbeSlice(probeSlice []string) *TcpBanner {
+	b := NewTcpBanner(n.target.host, n.target.port)
+	for _, requestName := range probeSlice {
+		if IsInStrArr(n.usedProbeSlice, requestName) {
+			continue
+		}
+		b.Load(n.getTcpBanner(n.probeGroup[requestName]))
+		//如果端口未开放，则等待10s后重新连接
+		if b.status == Closed {
+			time.Sleep(time.Second * 10)
+			b.Load(n.getTcpBanner(n.probeGroup[requestName]))
+		}
+		logger.Printf("Target:%s,Probe:%s,Status:%s,Service:%s,Response:%s", b.Target.URI(), requestName, b.StatusDisplay(), b.TcpFinger.Service, strconv.Quote(b.Response.string))
+		if b.status == Closed || b.status == Matched {
+			break
+		}
+		if n.target.port == 53 {
+			if DnsScan(n.target.URI()) {
+				b.TcpFinger.Service = "dns"
+				b.Response.string = "dns"
+				b.MATCHED()
+			} else {
+				b.CLOSED()
+			}
+			break
+		}
+	}
+	return &b
+}
+
+func (n *Nmap) fixFallback() {
+	for probeName, probeType := range n.probeGroup {
+		fallback := probeType.fallback
+		if fallback == "" {
+			continue
+		}
+		if _, ok := n.probeGroup["TCP_"+fallback]; ok {
+			n.probeGroup[probeName].fallback = "TCP_" + fallback
+		} else {
+			n.probeGroup[probeName].fallback = "UDP_" + fallback
+		}
+	}
+}
