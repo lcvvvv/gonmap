c0ea6ade8fa5dee5d1d12f0e06a9785f2ac64272
kv2 <cheng.kv2@gmail.com>
2022-04-08 18:47:43 +0800

    [*]优化标题获取逻辑，现在更人性化了

--- type-httpfinger.go
+++ type-httpfinger.go
@@ -64,6 +64,92 @@ func (h *HttpFinger) LoadHttpResponse(url *urlparse.URL, resp *http.Response) {
 	h.Finger.ApplicationComponent = strings.Join(componentSlice, ",")
 	//h.HashFinger = getFingerByHash(*url)
 	//h.KeywordFinger = getFingerByKeyword(h.Header, h.Title, h.Response)
+	if h.Title == "" {
+		switch h.StatusCode {
+		case 100:
+			h.Title = "100 Continue"
+		case 101:
+			h.Title = "101 Switching Protocols"
+		case 201:
+			h.Title = "201 Created"
+		case 202:
+			h.Title = "202 Accepted"
+		case 203:
+			h.Title = "203 Non-Authoritative Information"
+		case 204:
+			h.Title = "204 No Content"
+		case 205:
+			h.Title = "205 Reset Content"
+		case 206:
+			h.Title = "206 Partial Content"
+		case 300:
+			h.Title = "300 Multiple Choices"
+		case 301:
+			h.Title = "301 Moved Permanently"
+		case 302:
+			h.Title = "302 Found"
+		case 303:
+			h.Title = "303 See Other"
+		case 304:
+			h.Title = "304 Not Modified"
+		case 305:
+			h.Title = "305 Use Proxy"
+		case 306:
+			h.Title = "306 Unused"
+		case 307:
+			h.Title = "307 Temporary Redirect"
+		case 400:
+			h.Title = "400 Bad Request"
+		case 401:
+			h.Title = "401 Unauthorized"
+		case 402:
+			h.Title = "402 Payment Required"
+		case 403:
+			h.Title = "403 Forbidden"
+		case 404:
+			h.Title = "404 Not Found"
+		case 405:
+			h.Title = "405 Method Not Allowed"
+		case 406:
+			h.Title = "406 Not Acceptable"
+		case 407:
+			h.Title = "407 Proxy Authentication Required"
+		case 408:
+			h.Title = "408 Request Time-out"
+		case 409:
+			h.Title = "409 Conflict"
+		case 410:
+			h.Title = "410 Gone"
+		case 411:
+			h.Title = "411 Length Required"
+		case 412:
+			h.Title = "412 Precondition Failed"
+		case 413:
+			h.Title = "413 Request Entity Too Large"
+		case 414:
+			h.Title = "414 Request-URI Too Large"
+		case 415:
+			h.Title = "415 Unsupported Media Type"
+		case 416:
+			h.Title = "416 Requested range not satisfiable"
+		case 417:
+			h.Title = "417 Expectation Failed"
+		case 500:
+			h.Title = "500 Internal Server Error"
+		case 501:
+			h.Title = "501 Not Implemented"
+		case 502:
+			h.Title = "502 Bad Gateway"
+		case 503:
+			h.Title = "503 Service Unavailable"
+		case 504:
+			h.Title = "504 Gateway Time-out"
+		case 505:
+			h.Title = "505 HTTP Version not supported"
+		default:
+			h.Title = "No Title"
+		}
+	}
 	_ = resp.Body.Close()
 }
 
