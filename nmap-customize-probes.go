ea1242e4a87474b6af07f5c6d36ffcd770cc0dac
kv2 <cheng.kv2@gmail.com>
2022-06-07 17:50:41 +0800

    [+]新增JSONRPC探针及指纹

--- nmap-customize-probes.go
+++ nmap-customize-probes.go
@@ -6,4 +6,14 @@ rarity 1
 ports 445
 
 match microsoft-ds m|^\0\0...SMB.*|s
+
+
+Probe TCP JSON_RPC q|{"id":1,"jsonrpc":"2.0","method":"login","params":{}}\r\n|
+rarity 4
+ports 443,80,8443,8080
+
+match jsonrpc m|^{"jsonrpc":"([\d.]+)".*"height":(\d+),"seed_hash".*|s v/$1/ p/ETH/ i/height:$2/
+match jsonrpc m|^{"jsonrpc":"([\d.]+)".*|s v/$1/
+
+
 `
